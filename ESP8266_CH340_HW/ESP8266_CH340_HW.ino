f2261f3660a81396ba60dd4b15702d010317f77d
DanyloShchyrba <shchyrbadanylo@gmail.com>
2024-11-10 19:09:23 +0200

    lab 2

---
+++ ESP8266_CH340_HW/ESP8266_CH340_HW.ino
@@ -0,0 +1,10 @@
+void setup() {
+  Serial.begin(9600);
+}
+void loop() {
+  if (Serial.available() > 0) {
+    String message = Serial.readStringUntil('\n');
+    message += " - Modified by server";
+    Serial.println(message);
+    }
+}
